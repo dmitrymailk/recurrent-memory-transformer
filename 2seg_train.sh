d94763834990935cb86c70dabeca20b27a1c579b
_dimweb_ <dimweb.tech@mail.ru>
2025-07-26 01:17:20 +0000

    run original code, fix bugs

---
+++ 2seg_train.sh
@@ -0,0 +1,6 @@
+export http_proxy="127.0.0.1:2334"
+export https_proxy="127.0.0.1:2334"
+
+pushd /code/scripts_exp/babilong
+
+NP=1 ./finetune_babilong_qa1_rmt_vary_n_seg_iter_tasks_curriculum.sh
--- scripts_exp/babilong/finetune_babilong_qa1_rmt_vary_n_seg_iter_tasks_curriculum.sh
+++ scripts_exp/babilong/finetune_babilong_qa1_rmt_vary_n_seg_iter_tasks_curriculum.sh
@@ -18,98 +18,108 @@ MODEL_NAME=gpt2  # backbone model
 ITERS=5000
 TBS=64
 
-for TASK_DATASET in qa1_single-supporting-fact
+train_folder=/code/rmt_workdir
+model_base_folder="$train_folder"/babilong
+dataset_folder="$model_base_folder"/data/tasks_1-20_v1-2/en-10k
+noise_folder="$train_folder"/pg19
+
+mkdir -p $train_folder
+mkdir -p $model_base_folder
+mkdir -p $dataset_folder
+mkdir -p $noise_folder
+
+for TASK_DATASET in qa1_single-supporting-fact;
 do
-
-for LR in 1e-05
-do
-
-for SEGMENT_SIZE in 512
-do # size of one segment in tokens
-
-MAX_N_SEGMENTSS=(0 1 2 4 6 8 16 32)
-BSS=(32 32 16 16 8 8 4 2)
-
-for (( j=2; j<${#MAX_N_SEGMENTSS[@]}; j++ ))
-do
-MAX_N_SEGMENTS=${MAX_N_SEGMENTSS[j]} 
-BS=${BSS[j]}
-
-j1=$((j-1))
-SRC_N_SEGMENTS=${MAX_N_SEGMENTSS[j1]}
-
-j2=$((j-2))
-SRC_SRC_N_SEGMENTS=${MAX_N_SEGMENTSS[j2]}
-
-
-for MEMORY_SIZE in 16
-do
-
-SAMPLE_SIZE=$((MAX_N_SEGMENTS*SEGMENT_SIZE)) # length of task sample in tokens
-
-GRAD_ACC_STEPS=$(($TBS/($BS*$NP)))
-
-SCHEDULER=linear
-
-for N in 6
-do
-
-K2=-1   # BPTT unroll length
-
-NP=$NP
-ACCEL_CONFIG=/home/jovyan/rmt/babilong/accel_configs/accelerate/deepspeed_bf16_tbs${TBS}bs${BS}g${GRAD_ACC_STEPS}c1.0np${NP}.yaml
-cd accel_configs/
-python create_config.py \
-        --bf16 \
-        --train_batch_size $TBS\
-        --train_micro_batch_size_per_gpu $BS\
-        --gradient_accumulation_steps $GRAD_ACC_STEPS\
-        --np $NP\
-        --gradient_clipping 1.0
-cd ..
-
-echo RUNNING: TASK_DATASET $TASK_DATASET MEMORY_SIZE $MEMORY_SIZE SEGMENT_SIZE $SEGMENT_SIZE MAX_N_SEGMENTS $MAX_N_SEGMENTS
-echo SAMPLE_SIZE $SAMPLE_SIZE MODEL_NAME $MODEL_NAME  LR $LR N $N
-echo gradient accumulation steps $GRAD_ACC_STEPS
-
-accelerate launch --config_file $ACCEL_CONFIG --main_process_port 29007 run_finetuning_babilong_rmt.py \
-        --task_dataset $TASK_DATASET \
-        --noise_dataset $NOISE_DATASET \
-        --babi_path /home/jovyan/rmt/babilong/data/tasks_1-20_v1-2/en-10k \
-        --model_path /home/jovyan/rmt/runs/babilong/${TASK_DATASET}/$MODEL_NAME/${SCHEDULER}_adamw_wd1e-03_${MAX_N_SEGMENTS}x${SEGMENT_SIZE}_mem${MEMORY_SIZE}_bs${TBS}_bptt-${K2}_from_cpt_${SRC_N_SEGMENTS}-${MAX_N_SEGMENTS}/run_$N \
-        --model_cpt /home/jovyan/rmt/runs/babilong/${TASK_DATASET}/$MODEL_NAME/${SCHEDULER}_adamw_wd1e-03_${SRC_N_SEGMENTS}x${SEGMENT_SIZE}_mem${MEMORY_SIZE}_bs${TBS}_bptt-${K2}_from_cpt_${SRC_SRC_N_SEGMENTS}-${SRC_N_SEGMENTS}/run_$N/model_best \
-        --from_pretrained $MODEL_NAME \
-        --model_type $MODEL_TYPE \
-        --memory_cell_cls $MEMORY_CELL \
-        --recurrent_wrapper_cls $RECURRENT_WRAPPER \
-        --model_cls $BACKBONE_CLS \
-        --segment_size $SEGMENT_SIZE \
-        --sample_size $SAMPLE_SIZE \
-        --num_mem_tokens $MEMORY_SIZE \
-        --max_n_segments $MAX_N_SEGMENTS\
-        --vary_n_segments \
-        --batch_size $BS --gradient_accumulation_steps $(($TBS/($BS*$NP))) \
-        --num_training_steps $((ITERS*2)) \
-        --iters $ITERS \
-        --use_generate_on_valid \
-        --save_best \
-        --k2 $K2 \
-        --optimizer AdamW  --weight_decay 0.01 \
-        --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/10)) \
-        --data_n_workers 2 \
-        --log_interval $(($ITERS/100)) --valid_interval $(($ITERS/20)) \
-        --optimize_metric $METRIC --optimize_mode max \
-        --show_valid_examples 5 \
-        --early_stopping_patience 15 \
-        --seed $(($N+42)) \
-        --clip_grad_norm 1.0
+  
+  for LR in 1e-05;
+  do
+    
+    for SEGMENT_SIZE in 512;
+    do # size of one segment in tokens
+      
+      MAX_N_SEGMENTSS=(0 1 2 4 6 8 16 32)
+      BSS=(32 32 16 16 8 8 4 2)
+      
+      for (( j=2; j<${#MAX_N_SEGMENTSS[@]}; j++ ));
+      do
+        MAX_N_SEGMENTS=${MAX_N_SEGMENTSS[j]}
+        BS=${BSS[j]}
         
+        j1=$((j-1))
+        SRC_N_SEGMENTS=${MAX_N_SEGMENTSS[j1]}
+        
+        j2=$((j-2))
+        SRC_SRC_N_SEGMENTS=${MAX_N_SEGMENTSS[j2]}
+        
+        
+        for MEMORY_SIZE in 16;
+        do
+          
+          SAMPLE_SIZE=$((MAX_N_SEGMENTS*SEGMENT_SIZE)) # length of task sample in tokens
+          
+          GRAD_ACC_STEPS=$(($TBS/($BS*$NP)))
+          
+          SCHEDULER=linear
+          
+          for N in 6;
+          do
+            
+            K2=-1   # BPTT unroll length
+            
+            NP=$NP
+            #     ACCEL_CONFIG=/home/jovyan/rmt/babilong/accel_configs/accelerate/deepspeed_bf16_tbs${TBS}bs${BS}g${GRAD_ACC_STEPS}c1.0np${NP}.yaml
+            ACCEL_CONFIG=/code/accel_configs/accelerate/deepspeed_bf16_tbs${TBS}bs${BS}g${GRAD_ACC_STEPS}c1.0np${NP}.yaml
+            cd accel_configs/
+            python create_config.py \
+            --bf16 \
+            --train_batch_size $TBS\
+            --train_micro_batch_size_per_gpu $BS\
+            --gradient_accumulation_steps $GRAD_ACC_STEPS\
+            --np $NP\
+            --gradient_clipping 1.0
+            cd ..
+            
+            echo RUNNING: TASK_DATASET $TASK_DATASET MEMORY_SIZE $MEMORY_SIZE SEGMENT_SIZE $SEGMENT_SIZE MAX_N_SEGMENTS $MAX_N_SEGMENTS
+            echo SAMPLE_SIZE $SAMPLE_SIZE MODEL_NAME $MODEL_NAME  LR $LR N $N
+            echo gradient accumulation steps $GRAD_ACC_STEPS
+            
+            accelerate launch --config_file $ACCEL_CONFIG --main_process_port 29007 run_finetuning_babilong_rmt.py \
+            --task_dataset $TASK_DATASET \
+            --noise_dataset $NOISE_DATASET \
+            --babi_path $dataset_folder \
+            --model_path $model_base_folder/${TASK_DATASET}/$MODEL_NAME/${SCHEDULER}_adamw_wd1e-03_${MAX_N_SEGMENTS}x${SEGMENT_SIZE}_mem${MEMORY_SIZE}_bs${TBS}_bptt-${K2}_from_cpt_${SRC_N_SEGMENTS}-${MAX_N_SEGMENTS}/run_$N \
+            --from_pretrained $MODEL_NAME \
+            --model_type $MODEL_TYPE \
+            --memory_cell_cls $MEMORY_CELL \
+            --recurrent_wrapper_cls $RECURRENT_WRAPPER \
+            --model_cls $BACKBONE_CLS \
+            --segment_size $SEGMENT_SIZE \
+            --sample_size $SAMPLE_SIZE \
+            --num_mem_tokens $MEMORY_SIZE \
+            --max_n_segments $MAX_N_SEGMENTS\
+            --vary_n_segments \
+            --batch_size $BS --gradient_accumulation_steps $(($TBS/($BS*$NP))) \
+            --num_training_steps $((ITERS*2)) \
+            --iters $ITERS \
+            --save_best \
+            --k2 $K2 \
+            --optimizer AdamW  --weight_decay 0.01 \
+            --lr ${LR} --lr_scheduler $SCHEDULER --num_warmup_steps $(($ITERS/10)) \
+            --data_n_workers 2 \
+            --log_interval $(($ITERS/100)) --valid_interval $(($ITERS/20)) \
+            --optimize_metric $METRIC --optimize_mode max \
+            --show_valid_examples 5 \
+            --early_stopping_patience 15 \
+            --seed $(($N+42)) \
+            --clip_grad_norm 1.0 \
+            --opt_level "opt_1_MEM_SIZE_$MEMORY_SIZE SEG_SIZE_$SEGMENT_SIZE MAX_N_SEG_$MAX_N_SEGMENTS"
+            # --use_generate_on_valid \
+            
+          done
+        done
+      done
+    done
+  done
 done
-done
-done
-done
-done
-done
-done
-done
+
 echo "done"
